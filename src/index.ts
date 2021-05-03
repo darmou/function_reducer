02b620628efb4f42e3af8c65259696be7882e0b8
darmou <dmoulder@qtome.com>
2021-05-02 17:32:47 -0700

    cache calls

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
+import hash from "object-hash";
+
+const cache: any = {};
+interface Key  {
+    params: any,
+    action_fn_name: string,
+    state_md5: string
+}
+
+//Simple memoization
+const functionCache = (params: any, action_fn: any, state: any) => {
+    const keyObj: Key = {
+        params,
+        action_fn_name: action_fn.name,
+        state_md5: hash(state)
+    }
+    const keyStr = JSON.stringify(keyObj);
+    if (!cache.hasOwnProperty(keyStr)) {
+        cache[keyStr] = action_fn({state, ...params});
+    }
+    return cache[keyStr];
+}
+
 const functionReducer = (state: any | null, action: any) => {
   const {action_fn, ...params} = action;
   if  (typeof action_fn === "function") {
-      return  action_fn({state, ...params});
+      return functionCache(params, action_fn, state);
   } else {
       return state;
   }
